89707168243dc5f43a17440ed0cb901de5ff4d9a
Kamaraj <Vasanthakumar.Kamaraj@resideo.com>
2022-10-27 13:01:12 +0000

    News blog with Category and Language filters

--- src/components/pages/news/Newsitem.jsx
+++ src/components/pages/news/Newsitem.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import noImage from '../../../images/no-image.jpg';
 
 const Newsitem = ({title, description, imageurl, linkurl}) => {
     return(
         <>
             <div className="card" >
-                <img className="card-img-top" src={imageurl} alt={title} />
+                <img className="card-img-top" 
+                    src={imageurl != null && imageurl.length > 0 ? imageurl : noImage }
+                    alt={title} 
+                />
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
--- src/components/pages/news/Newslist.jsx
+++ src/components/pages/news/Newslist.jsx
@@ -9,30 +9,54 @@ function Newslist() {
         url: '',
         urlToImage: '',
         publishedAt:''
-    }])   
+    }])
+    
+    const categories = ['business', 'entertainment', 'health', 'science', 'sports', 'technology'];
+    const[category, setCategory] = useState('business')
+    const handleCategoryChange = (e) => {
+        setCategory(e.target.value)
+    }  
+    const languages = ['ar','de','en', 'es', 'fr', 'he', 'it', 'in', 'nl', 'no', 'pt', 'ru', 'sv', 'ud', 'zh'];
+    const[language, setLanguage] = useState('in') 
+    const handleLanguageChange = (e) => {
+        setLanguage(e.target.value)
+    } 
 
     useEffect(()=> {        
-        const uri = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=a12fe2b2731340be93b7e3c2ecad4d5d`;
+        const uri = `https://newsapi.org/v2/top-headlines?country=${language}&category=${category}&apiKey=a12fe2b2731340be93b7e3c2ecad4d5d`;
         
         const getAriticles = async() => {
             const response = await axios.get(uri)
-            console.log(response)
             setNewsdata(response.data.articles)
         }
-        getAriticles()
+        getAriticles()        
         
-    }, [])
+    }, [category, language])    
+    
     
     return(
         <>
-            <h1 className='display-4'>Blogs</h1>
-            <div className='sort col-md-3 offset-md-7'>
-                <select class="form-select" aria-label="Default select example">
-                    <option selected>Category</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </select>
+        <div className='container'>
+        <h1 className='display-4'>Blogs</h1>
+            <div className='row'>
+                <div className='sort col-md-3'>
+                    <select id="languages" onChange={handleLanguageChange} className="form-select" aria-label="Default select example">
+                        {languages.map((item, i) => (
+                            <option value={item} key={i}>
+                                {item}
+                            </option>
+                        ))}                    
+                    </select>
+                </div>
+                <div className='sort col-md-3'>
+                    <select id="categories" onChange={handleCategoryChange} className="form-select" aria-label="Default select example">
+                        {categories.map((item, i) => (
+                            <option value={item} key={i}>
+                                {item}
+                            </option>
+                        ))}                    
+                    </select>
+                </div>
             </div>
             <div className='cards-container'>
             {             
@@ -52,6 +76,7 @@ function Newslist() {
                 })
             }
             </div>
+        </div>           
             
         </>
     )
