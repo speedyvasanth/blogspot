ed55d0c2ae678526c7f32edf1238d2fc28adb021
Kamaraj <Vasanthakumar.Kamaraj@resideo.com>
2022-10-29 07:05:49 +0000

    Amazon Amplify

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -4,6 +4,7 @@ function Navbar() {
 
     return(
         <>
+        <h1>Amazon Amplify</h1>
         <div className='navBar'>
             <a href="/createBlog">Create Blog</a>
             <a href="/blogs">View All Blogs</a>
